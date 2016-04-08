dc1c0a302ae853c6d9844ccfdca037605094500b
Mickael <mickael.phanthavong@garcia-rochette.com>
2016-04-08 09:53:38 +0200

    test

---
+++ target/scala-2.11/routes/main/router/RoutesPrefix.scala
@@ -0,0 +1,16 @@
+
+// @GENERATOR:play-routes-compiler
+// @SOURCE:/Users/phanthavong/play-java/conf/routes
+// @DATE:Fri Apr 08 09:14:03 CEST 2016
+
+
+package router {
+  object RoutesPrefix {
+    private var _prefix: String = "/"
+    def setPrefix(p: String): Unit = {
+      _prefix = p
+    }
+    def prefix: String = _prefix
+    val byNamePrefix: Function0[String] = { () => prefix }
+  }
+}

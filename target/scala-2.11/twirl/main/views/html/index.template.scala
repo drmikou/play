8dba08aed55c51784bc6fdcbfe5db565a86b2135
Mickael <mickael.phanthavong@garcia-rochette.com>
2016-04-08 11:45:25 +0200

    ignore

--- target/scala-2.11/twirl/main/views/html/index.template.scala
+++ target/scala-2.11/twirl/main/views/html/index.template.scala
@@ -35,10 +35,10 @@ class index extends BaseScalaTemplate[play.twirl.api.HtmlFormat.Appendable,Forma
 Seq[Any](format.raw/*5.19*/("""
 
 """),format.raw/*11.4*/("""
-"""),_display_(/*12.2*/main("Welcome to ddPlay")/*12.27*/ {_display_(Seq[Any](format.raw/*12.29*/("""
+"""),_display_(/*12.2*/main("Salut")/*12.15*/ {_display_(Seq[Any](format.raw/*12.17*/("""
 
     """),format.raw/*17.8*/("""
-    """),_display_(/*18.6*/play20/*18.12*/.welcome(message, style = "Java")),format.raw/*18.45*/("""
+    
 
 """)))}),format.raw/*20.2*/("""
 """))
@@ -64,11 +64,11 @@ Seq[Any](format.raw/*5.19*/("""
 object index extends index_Scope0.index
               /*
                   -- GENERATED --
-                  DATE: Fri Apr 08 10:56:21 CEST 2016
+                  DATE: Fri Apr 08 11:36:40 CEST 2016
                   SOURCE: /Users/phanthavong/play-java/app/views/index.scala.html
-                  HASH: 84b605332f50186565ef87830f5b1d7d785366bb
-                  MATRIX: 834->95|946->112|975->312|1003->314|1037->339|1077->341|1110->470|1142->476|1157->482|1211->515|1244->518
-                  LINES: 30->5|35->5|37->11|38->12|38->12|38->12|40->17|41->18|41->18|41->18|43->20
+                  HASH: aa4cfb26b33268fb78650daa54764e5e0376f141
+                  MATRIX: 834->95|946->112|975->312|1003->314|1025->327|1065->329|1098->458|1136->466
+                  LINES: 30->5|35->5|37->11|38->12|38->12|38->12|40->17|43->20
                   -- GENERATED --
               */
           

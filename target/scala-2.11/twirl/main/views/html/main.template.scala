e77a9eec1d9a0312279ad76198e674442dbc23fb
Mickael <mickael.phanthavong@garcia-rochette.com>
2016-05-11 22:22:58 +0200

    accueil front

--- target/scala-2.11/twirl/main/views/html/main.template.scala
+++ target/scala-2.11/twirl/main/views/html/main.template.scala
@@ -31,38 +31,35 @@ class main extends BaseScalaTemplate[play.twirl.api.HtmlFormat.Appendable,Format
 
 Seq[Any](format.raw/*1.58*/("""
 
-"""),format.raw/*3.1*/("""<!DOCTYPE html>
+
+
+"""),format.raw/*5.1*/("""<!DOCTYPE html>
 <html lang="en">
 
 <head>
-
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>"""),_display_(/*14.13*/title),format.raw/*14.18*/("""</title>
+    <title>"""),_display_(/*15.13*/title),format.raw/*15.18*/("""</title>
 
     <!-- Bootstrap Core CSS -->
-    <link rel="stylesheet" media="screen" href=""""),_display_(/*17.50*/routes/*17.56*/.Assets.versioned("stylesheets/bootstrap.min.css")),format.raw/*17.106*/("""">
+    <link rel="stylesheet" media="screen" href=""""),_display_(/*18.50*/routes/*18.56*/.Assets.versioned("stylesheets/bootstrap.min.css")),format.raw/*18.106*/("""">
 
     <!-- Custom CSS -->
-    <link rel="stylesheet" media="screen" href=""""),_display_(/*20.50*/routes/*20.56*/.Assets.versioned("stylesheets/sb-admin.css")),format.raw/*20.101*/("""">
+    <link rel="stylesheet" media="screen" href=""""),_display_(/*21.50*/routes/*21.56*/.Assets.versioned("stylesheets/sb-admin.css")),format.raw/*21.101*/("""">
 
     <!-- Morris Charts CSS -->
-    <link rel="stylesheet" media="screen" href=""""),_display_(/*23.50*/routes/*23.56*/.Assets.versioned("stylesheets/morris.css")),format.raw/*23.99*/("""">
+    <link rel="stylesheet" media="screen" href=""""),_display_(/*24.50*/routes/*24.56*/.Assets.versioned("stylesheets/morris.css")),format.raw/*24.99*/("""">
 
     <!-- Custom Fonts -->
-    <link rel="stylesheet" media="screen" href=""""),_display_(/*26.50*/routes/*26.56*/.Assets.versioned("font-awesome/css/font-awesome.min.css")),format.raw/*26.114*/("""">
-
-
+    <link rel="stylesheet" media="screen" href=""""),_display_(/*27.50*/routes/*27.56*/.Assets.versioned("font-awesome/css/font-awesome.min.css")),format.raw/*27.114*/("""">
 </head>
 
 <body>
-
 <div id="wrapper">
-
     <!-- Navigation -->
     <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
         <!-- Brand and toggle get grouped for better mobile display -->
@@ -73,79 +70,17 @@ Seq[Any](format.raw/*1.58*/("""
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="index.html">GL Project</a>
+            <a class="navbar-brand" href=""""),_display_(/*42.44*/routes/*42.50*/.AccueilController.accueil),format.raw/*42.76*/("""">GL Project</a>
+            <a class="navbar-brand" href=""""),_display_(/*43.44*/routes/*43.50*/.AccueilController.accueil),format.raw/*43.76*/("""">
+                <i class="fa fa-home"></i>
+            </a>
         </div>
         <!-- Top Menu Items -->
         <ul class="nav navbar-right top-nav">
+
             <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-envelope"></i> <b class="caret"></b></a>
-                <ul class="dropdown-menu message-dropdown">
-                    <li class="message-preview">
-                        <a href="#">
-                            <div class="media">
-                                    <span class="pull-left">
-                                        <img class="media-object" src="http://placehold.it/50x50" alt="">
-                                    </span>
-                                <div class="media-body">
-                                    <h5 class="media-heading">
-                                        <strong>John Smith</strong>
-                                    </h5>
-                                    <p class="small text-muted"><i class="fa fa-clock-o"></i> Yesterday at 4:32 PM</p>
-                                    <p>Lorem ipsum dolor sit amet, consectetur...</p>
-                                </div>
-                            </div>
-                        </a>
-                    </li>
-                    <li class="message-preview">
-                        <a href="#">
-                            <div class="media">
-                                    <span class="pull-left">
-                                        <img class="media-object" src="http://placehold.it/50x50" alt="">
-                                    </span>
-                                <div class="media-body">
-                                    <h5 class="media-heading">
-                                        <strong>John Smith</strong>
-                                    </h5>
-                                    <p class="small text-muted"><i class="fa fa-clock-o"></i> Yesterday at 4:32 PM</p>
-                                    <p>Lorem ipsum dolor sit amet, consectetur...</p>
-                                </div>
-                            </div>
-                        </a>
-                    </li>
-                    <li class="message-preview">
-                        <a href="#">
-                            <div class="media">
-                                    <span class="pull-left">
-                                        <img class="media-object" src="http://placehold.it/50x50" alt="">
-                                    </span>
-                                <div class="media-body">
-                                    <h5 class="media-heading">
-                                        <strong>John Smith</strong>
-                                    </h5>
-                                    <p class="small text-muted"><i class="fa fa-clock-o"></i> Yesterday at 4:32 PM</p>
-                                    <p>Lorem ipsum dolor sit amet, consectetur...</p>
-                                </div>
-                            </div>
-                        </a>
-                    </li>
-                    <li class="message-footer">
-                        <a href="#">Read All New Messages</a>
-                    </li>
-                </ul>
-            </li>
-            <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> John Smith <b class="caret"></b></a>
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> User <b class="caret"></b></a>
                 <ul class="dropdown-menu">
-                    <li>
-                        <a href="#"><i class="fa fa-fw fa-user"></i> Profile</a>
-                    </li>
-                    <li>
-                        <a href="#"><i class="fa fa-fw fa-envelope"></i> Inbox</a>
-                    </li>
-                    <li>
-                        <a href="#"><i class="fa fa-fw fa-gear"></i> Settings</a>
-                    </li>
-                    <li class="divider"></li>
                     <li>
                         <a href="#"><i class="fa fa-fw fa-power-off"></i> Log Out</a>
                     </li>
@@ -155,11 +90,17 @@ Seq[Any](format.raw/*1.58*/("""
         <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
         <div class="collapse navbar-collapse navbar-ex1-collapse">
             <ul class="nav navbar-nav side-nav">
-                <li class="active">
-                    <a href="index.html"><i class="fa fa-fw fa-dashboard"></i> Accueil</a>
+                <li>
+                    <a href="#"><i class="fa fa-users"></i> Mon groupe</a>
                 </li>
                 <li>
-                    <a href="charts.html"><i class="fa fa-fw fa-bar-chart-o"></i> Charts</a>
+                    <a href="#"><i class="fa fa-calendar"></i> Calendrier</a>
+                </li>
+                <li class="">
+                    <a href="#"><i class="fa fa-folder-open"></i> Dépôt de documents</a>
+                </li>
+                <li>
+                    <a href="#"><i class="fa fa-envelope-o"></i> Messagerie</a>
                 </li>
 
             </ul>
@@ -168,18 +109,18 @@ Seq[Any](format.raw/*1.58*/("""
     </nav>
 
     <div id="page-wrapper">
-        """),_display_(/*140.10*/content),format.raw/*140.17*/("""
-    """),format.raw/*141.5*/("""</div>
+        """),_display_(/*81.10*/content),format.raw/*81.17*/("""
+    """),format.raw/*82.5*/("""</div>
     <!-- /#page-wrapper -->
 
 </div>
 <!-- /#wrapper -->
 
 <!-- jQuery -->
-<script src=""""),_display_(/*148.15*/routes/*148.21*/.Assets.versioned("javascripts/jquery.js")),format.raw/*148.63*/("""" type="text/javascript"></script>
+<script src=""""),_display_(/*89.15*/routes/*89.21*/.Assets.versioned("javascripts/jquery.js")),format.raw/*89.63*/("""" type="text/javascript"></script>
 
 
-<script src=""""),_display_(/*151.15*/routes/*151.21*/.Assets.versioned("javascripts/bootstrap.min.js")),format.raw/*151.70*/("""" type="text/javascript"></script>
+<script src=""""),_display_(/*92.15*/routes/*92.21*/.Assets.versioned("javascripts/bootstrap.min.js")),format.raw/*92.70*/("""" type="text/javascript"></script>
 
 </body>
 
@@ -204,11 +145,11 @@ Seq[Any](format.raw/*1.58*/("""
 object main extends main_Scope0.main
               /*
                   -- GENERATED --
-                  DATE: Tue May 10 16:03:14 CEST 2016
+                  DATE: Wed May 11 20:44:44 CEST 2016
                   SOURCE: /Users/phanthavong/play-java/app/views/main.scala.html
-                  HASH: 70c48af24a44121cbdff1c4c1fcac66579c309c1
-                  MATRIX: 753->1|904->57|932->59|1249->349|1275->354|1393->445|1408->451|1480->501|1584->578|1599->584|1666->629|1777->713|1792->719|1856->762|1962->841|1977->847|2057->905|7441->6261|7470->6268|7503->6273|7624->6366|7640->6372|7704->6414|7783->6465|7799->6471|7870->6520
-                  LINES: 27->1|32->1|34->3|45->14|45->14|48->17|48->17|48->17|51->20|51->20|51->20|54->23|54->23|54->23|57->26|57->26|57->26|171->140|171->140|172->141|179->148|179->148|179->148|182->151|182->151|182->151
+                  HASH: f919f11e676ced460f4b4582f6ab21c26a21b57d
+                  MATRIX: 753->1|904->57|934->61|1250->350|1276->355|1394->446|1409->452|1481->502|1585->579|1600->585|1667->630|1778->714|1793->720|1857->763|1963->842|1978->848|2058->906|2714->1535|2729->1541|2776->1567|2863->1627|2878->1633|2925->1659|4351->3058|4379->3065|4411->3070|4531->3163|4546->3169|4609->3211|4687->3262|4702->3268|4772->3317
+                  LINES: 27->1|32->1|36->5|46->15|46->15|49->18|49->18|49->18|52->21|52->21|52->21|55->24|55->24|55->24|58->27|58->27|58->27|73->42|73->42|73->42|74->43|74->43|74->43|112->81|112->81|113->82|120->89|120->89|120->89|123->92|123->92|123->92
                   -- GENERATED --
               */
           

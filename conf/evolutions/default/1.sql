9d3f8007ed1ef786fa9824f3c6aff13eda3c98b3
Optik <thomas.poleya@isep.fr>
2016-05-27 08:52:21 +0200

    comm

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -4,80 +4,71 @@
 # --- !Ups
 
 create table depot (
-  id                            bigint not null,
+  id                            bigint auto_increment not null,
   nom                           varchar(255),
   commentaire                   varchar(255),
-  datedepot                     timestamp,
+  datedepot                     datetime(6),
   equipe_id                     bigint,
   constraint pk_depot primary key (id)
 );
-create sequence depot_seq;
 
 create table equipe (
-  id                            bigint not null,
+  id                            bigint auto_increment not null,
   nom                           varchar(255),
   groupe_id                     bigint,
   constraint pk_equipe primary key (id)
 );
-create sequence equipe_seq;
 
 create table fonctionnalite (
-  id                            bigint not null,
+  id                            bigint auto_increment not null,
   nom                           varchar(255),
   equipe_id                     bigint,
-  trouve                        boolean,
+  trouve                        tinyint(1) default 0,
   constraint pk_fonctionnalite primary key (id)
 );
-create sequence fonctionnalite_seq;
 
 create table groupe (
-  id                            bigint not null,
+  id                            bigint auto_increment not null,
   nom                           varchar(255),
   session_id                    bigint,
   constraint pk_groupe primary key (id)
 );
-create sequence groupe_seq;
 
 create table projet (
-  id                            bigint not null,
+  id                            bigint auto_increment not null,
   nom                           varchar(255),
   equipe_id                     bigint,
   constraint pk_projet primary key (id)
 );
-create sequence projet_seq;
 
 create table promotion (
-  id                            bigint not null,
+  id                            bigint auto_increment not null,
   annee                         integer,
   constraint pk_promotion primary key (id)
 );
-create sequence promotion_seq;
 
 create table reunion (
-  id                            bigint not null,
+  id                            bigint auto_increment not null,
   nom                           varchar(255),
-  date                          timestamp,
+  date                          datetime(6),
   equipe_id                     bigint,
   constraint pk_reunion primary key (id)
 );
-create sequence reunion_seq;
 
 create table session (
-  id                            bigint not null,
+  id                            bigint auto_increment not null,
   nom                           varchar(255),
   promotion_id                  bigint,
   constraint pk_session primary key (id)
 );
-create sequence session_seq;
 
 create table user (
-  id                            bigint not null,
+  id                            bigint auto_increment not null,
   nom                           varchar(255),
   prenom                        varchar(255),
   equipe_id                     bigint,
   constraint pk_user primary key (id)
 );
-create sequence user_seq;
 
 alter table depot add constraint fk_depot_equipe_id foreign key (equipe_id) references equipe (id) on delete restrict on update restrict;
 create index ix_depot_equipe_id on depot (equipe_id);
@@ -106,54 +97,45 @@ create index ix_user_equipe_id on user (equipe_id);
 
 # --- !Downs
 
-alter table depot drop constraint if exists fk_depot_equipe_id;
-drop index if exists ix_depot_equipe_id;
+alter table depot drop foreign key fk_depot_equipe_id;
+drop index ix_depot_equipe_id on depot;
 
-alter table equipe drop constraint if exists fk_equipe_groupe_id;
-drop index if exists ix_equipe_groupe_id;
+alter table equipe drop foreign key fk_equipe_groupe_id;
+drop index ix_equipe_groupe_id on equipe;
 
-alter table fonctionnalite drop constraint if exists fk_fonctionnalite_equipe_id;
-drop index if exists ix_fonctionnalite_equipe_id;
+alter table fonctionnalite drop foreign key fk_fonctionnalite_equipe_id;
+drop index ix_fonctionnalite_equipe_id on fonctionnalite;
 
-alter table groupe drop constraint if exists fk_groupe_session_id;
-drop index if exists ix_groupe_session_id;
+alter table groupe drop foreign key fk_groupe_session_id;
+drop index ix_groupe_session_id on groupe;
 
-alter table projet drop constraint if exists fk_projet_equipe_id;
-drop index if exists ix_projet_equipe_id;
+alter table projet drop foreign key fk_projet_equipe_id;
+drop index ix_projet_equipe_id on projet;
 
-alter table reunion drop constraint if exists fk_reunion_equipe_id;
-drop index if exists ix_reunion_equipe_id;
+alter table reunion drop foreign key fk_reunion_equipe_id;
+drop index ix_reunion_equipe_id on reunion;
 
-alter table session drop constraint if exists fk_session_promotion_id;
-drop index if exists ix_session_promotion_id;
+alter table session drop foreign key fk_session_promotion_id;
+drop index ix_session_promotion_id on session;
 
-alter table user drop constraint if exists fk_user_equipe_id;
-drop index if exists ix_user_equipe_id;
+alter table user drop foreign key fk_user_equipe_id;
+drop index ix_user_equipe_id on user;
 
 drop table if exists depot;
-drop sequence if exists depot_seq;
 
 drop table if exists equipe;
-drop sequence if exists equipe_seq;
 
 drop table if exists fonctionnalite;
-drop sequence if exists fonctionnalite_seq;
 
 drop table if exists groupe;
-drop sequence if exists groupe_seq;
 
 drop table if exists projet;
-drop sequence if exists projet_seq;
 
 drop table if exists promotion;
-drop sequence if exists promotion_seq;
 
 drop table if exists reunion;
-drop sequence if exists reunion_seq;
 
 drop table if exists session;
-drop sequence if exists session_seq;
 
 drop table if exists user;
-drop sequence if exists user_seq;
 
